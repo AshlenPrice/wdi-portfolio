43111530f1173a288ab408348814f0e4493a4c9a
ashlenprice <ashlen.price@gmail.com>
2017-04-17 14:39:57 -0400

    All Setup complete
    Didn't end up being a vcard like I tried testing early on,
    but I'm satisfied.
    
    Separated all sections of styling into different files

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,15 +1,16 @@
 'use strict';
 
-$(function () {
+// $(function () {
+//
+//   var links = $('.sidebar-links > a');
+//
+//   links.on('click', function () {
+//
+//     links.removeClass('selected');
+//     $(this).addClass('selected');
+//
+//   })
+// });
 
-  var links = $('.sidebar-links > a');
-
-  links.on('click', function () {
-
-    links.removeClass('selected');
-    $(this).addClass('selected');
-
-  })
-});
 
 module.exports = true;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,8 +6,8 @@ const config = require('./config');
 $(() => {
   setAPIOrigin(location, config);
 
-});
 
+});
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
 
